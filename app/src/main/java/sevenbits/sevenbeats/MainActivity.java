6bd5855f8688a5b20cf554c34c0c589f70530c93
Pablololo12 <pabloheralm@gmail.com>
2016-02-28 22:25:01 +0100

    Añadidos elementos de song edit

--- app/src/main/java/sevenbits/sevenbeats/MainActivity.java
+++ app/src/main/java/sevenbits/sevenbeats/MainActivity.java
@@ -1,5 +1,6 @@
 package sevenbits.sevenbeats;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -22,4 +23,24 @@ public class MainActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.MainActivity_boton_busqueda:
+                return true;
+            case R.id.MainActivity_boton_anyadir:
+                anyadir();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public boolean anyadir(){
+        Intent i = new Intent(this, SongEdit.class);
+        startActivity(i);
+        return true;
+    }
 }
