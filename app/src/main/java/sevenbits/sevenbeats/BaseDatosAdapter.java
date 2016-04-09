4c5a68bc491d5871978883df4b0430436a6497b2
Eduardo Criado <ecriado95@gmail.com>
2016-04-09 19:53:43 +0200

    Avanzada MainActivity. Creada la clase BaseDatosAdapter para poder llamar a metodos.

---
+++ app/src/main/java/sevenbits/sevenbeats/BaseDatosAdapter.java
@@ -0,0 +1,13 @@
+package sevenbits.sevenbeats;
+
+import android.database.Cursor;
+
+/**
+ * Created by eduardo on 9/04/16.
+ */
+public class BaseDatosAdapter {
+
+    public Cursor fetchAllSongs() {
+        return null;
+    }
+}
--- app/src/main/java/sevenbits/sevenbeats/MainActivity.java
+++ app/src/main/java/sevenbits/sevenbeats/MainActivity.java
@@ -1,18 +1,30 @@
 package sevenbits.sevenbeats;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.SimpleCursorAdapter;
 
+/**
+ * Tengo que tener un menu lateral que hay que rellenar con
+ * las distintas opciones. De primeras se rellena con las canciones.
+ *
+ */
 public class MainActivity extends AppCompatActivity {
+
+    private BaseDatosAdapter bbdd;
+    private static final String CANCION_NOMBRE = "nombre";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // rellenar lista lateral y lista central
         setContentView(R.layout.main_activity);
+
     }
 
     @Override
@@ -42,5 +54,13 @@ public class MainActivity extends AppCompatActivity {
         Intent i = new Intent(this, SongEdit.class);
         startActivity(i);
         return true;
+    }
+
+    private void fillSongData() {
+        Cursor cursor = bbdd.fetchAllSongs();
+        String[] fromColumns = {CANCION_NOMBRE};
+        int[] toViews = {R.id.MainActivity_lista_principal};
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.main_activity, cursor,
+                fromColumns, toViews, 0);
     }
 }
