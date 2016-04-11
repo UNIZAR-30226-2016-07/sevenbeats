a5b424b9016fc7ea5d8e219f44d399404f41c22b
Fernando Alegre <Fernando Alegre>
2016-04-11 16:32:28 +0200

    seesong y songedit a mitad

--- app/src/main/java/sevenbits/sevenbeats/SeeSong.java
+++ app/src/main/java/sevenbits/sevenbeats/SeeSong.java
@@ -1,16 +1,101 @@
 package sevenbits.sevenbeats;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.media.Image;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URL;
 
 public class SeeSong extends AppCompatActivity {
+
+    private BaseDatosAdapter dbHelper;
+    private Cursor mNotesCursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_see_song);
+
+        //Capturo los parametros
+        Bundle extras = getIntent().getExtras();
+
+        final String nombreCancion = extras.getString("SeeCancion_cancion");
+
+        /*Llamo a la base de datos para capturar los datos que necesito*/
+        final String rutaImagen = dbHelper.imagen(nombreCancion);
+        String artista = dbHelper.artista(nombreCancion);
+        String genero = dbHelper.generos(nombreCancion).getString(1);
+        String duracion = dbHelper.duracion(nombreCancion);
+
+        /*Asigno cada valor a sus correspondientes variables*/
+        TextView asignador = (TextView)findViewById(R.id.nombreAlbum);
+        asignador.setText(nombreCancion);
+
+        asignador = (TextView)findViewById(R.id.artistaAlbum);
+        asignador.setText(artista);
+
+        asignador = (TextView)findViewById(R.id.generoAlbum);
+        asignador.setText(genero);
+
+        /*Si no hay caratula, enseñar imagen por defecto*/
+        ImageView imagen = (ImageView)findViewById(R.id.imageViewAlbum);
+        if ( rutaImagen != null ){
+            imagen.setImageURI(Uri.parse(rutaImagen));
+        }
+        else{
+            imagen.setImageURI(Uri.parse(imagenDefecto));
+        }
+
+        final Context activity = this;
+        final EditText txtUrl = new EditText(activity);
+
+        final Button button = (Button) findViewById(R.id.buttonAlbum);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                // Set the default text to a link of the Queen
+                txtUrl.setHint("Pon aqui la url de la imagen");
+
+                new AlertDialog.Builder(activity)
+                        .setTitle("Cargar caratula")
+                        .setMessage("Escribe la caratula de la imagen para poderla descargar")
+                        .setView(txtUrl)
+                        .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                String url = txtUrl.getText().toString();
+                                ponerCaratula(url, nombreAlbum);
+                                ImageView imagen = (ImageView)findViewById(R.id.imageViewAlbum);
+                                imagen.setImageURI(Uri.parse(rutaImagen));
+
+                            }
+
+                        })
+                        .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                            }
+                        })
+                        .show();
+            }
+        });
+
     }
 
     @Override
@@ -33,5 +118,39 @@ public class SeeSong extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Coge una imagen de internet y la guarda en la carpeta drawable. Luego, anota
+     * esa caratula a la base de datos.
+     *
+     * @return true si solo si el proceso se ha realizado de forma correcta.
+     */
+    public boolean ponerCaratula(String ruta, String nombreAlbum){
+        Image image = null;
+        try {
+            URL url = new URL(ruta);
+            InputStream in = new BufferedInputStream(url.openStream());
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            byte[] buf = new byte[1024];
+            int n = 0;
+            while (-1!=(n=in.read(buf)))
+            {
+                out.write(buf, 0, n);
+            }
+            out.close();
+            in.close();
+            byte[] response = out.toByteArray();
+
+            FileOutputStream fos = new FileOutputStream("drawable://" + nombreAlbum + ".jpg");
+            fos.write(response);
+            fos.close();
+
+            dbHelper.anadirCaratula(nombreAlbum, nombreAlbum + ".jpg");
+
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
     }
 }
--- app/src/main/java/sevenbits/sevenbeats/SongEdit.java
+++ app/src/main/java/sevenbits/sevenbeats/SongEdit.java
@@ -1,16 +1,79 @@
 package sevenbits.sevenbeats;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Button;
+import android.view.View;
 
 public class SongEdit extends AppCompatActivity {
+
+    private EditText SongEdit_texto_titulo;
+    private EditText SongEdit_texto_album;
+    private Integer SongEdit_texto_valoracion;
+    private EditText SongEdit_texto_duracion;
+    private Long mRowId;
+
+    private BaseDatosAdapter dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        dbHelper = new BaseDatosAdapter(this);
+        dbHelper.open();
+
         setContentView(R.layout.activity_song_edit);
+        setTitle("Editar cancion");
+
+        SongEdit_texto_titulo = (EditText) findViewById(R.id.titulo);
+        SongEdit_texto_album = (EditText) findViewById(R.id.album);
+        SongEdit_texto_valoracion = (EditText) findViewById(R.id.valoracion);
+        SongEdit_texto_duracion = (EditText) findViewById(R.id.duracion);
+
+        Button SongEdit_boton_guardar = (Button) findViewById(R.id.confirm);
+
+        mRowId = (savedInstanceState == null) ? null :
+                (Long) savedInstanceState.getSerializable("_id");
+        if (mRowId == null) {
+            Bundle extras = getIntent().getExtras();
+            mRowId = (extras != null) ? extras.getLong("_id")
+                    : null;
+        }
+        populateFields();
+        SongEdit_boton_guardar.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                setResult(RESULT_OK);
+                finish();
+            }
+
+        });
+    }
+
+    private void populateFields() {
+        if (mRowId != null) {
+            Cursor note = dbHelper.fetchCancion(mRowId);
+            startManagingCursor(note);
+            SongEdit_texto_titulo.setText(note.getString(
+                    note.getColumnIndexOrThrow("titulo")));
+            SongEdit_texto_album.setText(note.getString(
+                    note.getColumnIndexOrThrow("album")));
+            SongEdit_texto_valoracion.setText(note.getString(
+                    note.getColumnIndexOrThrow("valoracion")));
+            SongEdit_texto_duracion.setText(note.getString(
+                    note.getColumnIndexOrThrow("duracion")));
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        saveState();
+        outState.putSerializable("_id", mRowId);
     }
 
     @Override
@@ -33,5 +96,21 @@ public class SongEdit extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void saveState() {
+        String titulo = SongEdit_texto_titulo.getText().toString();
+        String album = SongEdit_texto_album.getText().toString();
+        Integer valoracion = SongEdit_texto_valoracion.getText().toString();
+        String duracion = SongEdit_texto_duracion.getText().toString();
+
+        if (mRowId == null) {
+            long id = dbHelper.createCancion(titulo, duracion, valoracion, album);
+            if (id > 0) {
+                mRowId = id;
+            }
+        } else {
+            dbHelper.updateCancion(mRowId, titulo, duracion, valoracion, album);
+        }
     }
 }
